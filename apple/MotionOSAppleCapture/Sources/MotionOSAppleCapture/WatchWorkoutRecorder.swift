40404abf346c6b55c58908113316aeb5ce809230
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-21 18:40:08 -0700

    feat: support mirrored watch workout lifecycle

--- apple/MotionOSAppleCapture/Sources/MotionOSAppleCapture/WatchWorkoutRecorder.swift
+++ apple/MotionOSAppleCapture/Sources/MotionOSAppleCapture/WatchWorkoutRecorder.swift
@@ -3,10 +3,23 @@ import Foundation
 import HealthKit
 
 public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKLiveWorkoutBuilderDelegate {
+    public enum State: Sendable, Equatable {
+        case idle
+        case starting
+        case running
+        case paused
+        case ending
+        case ended
+        case failed(String)
+    }
+
     private let healthStore: HKHealthStore
     private var session: HKWorkoutSession?
     private var builder: HKLiveWorkoutBuilder?
+
     public var onHeartRateBPM: (@Sendable (Double, UInt64) -> Void)?
+    public var onStateChange: (@Sendable (State) -> Void)?
+    public var onWorkoutFinished: (@Sendable (HKWorkout?) -> Void)?
 
     public init(healthStore: HKHealthStore = HKHealthStore()) {
         self.healthStore = healthStore
@@ -20,10 +33,13 @@ public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKL
         )
     }
 
-    public func start(activity: HKWorkoutActivityType = .other) throws {
-        let configuration = HKWorkoutConfiguration()
-        configuration.activityType = activity
-        configuration.locationType = .outdoor
+    public func start(
+        configuration: HKWorkoutConfiguration,
+        mirrorToCompanion: Bool = true
+    ) async throws {
+        guard session == nil else { throw RecorderError.alreadyRunning }
+        onStateChange?(.starting)
+
         let session = try HKWorkoutSession(
             healthStore: healthStore,
             configuration: configuration
@@ -37,12 +53,52 @@ public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKL
         builder.delegate = self
         self.session = session
         self.builder = builder
+
+        if mirrorToCompanion {
+            do {
+                try await session.startMirroringToCompanionDevice()
+            } catch {
+                self.session = nil
+                self.builder = nil
+                onStateChange?(.failed(error.localizedDescription))
+                throw error
+            }
+        }
+
         let start = Date()
         session.startActivity(with: start)
-        builder.beginCollection(withStart: start) { _, _ in }
+        do {
+            try await builder.beginCollection(at: start)
+        } catch {
+            session.end()
+            self.session = nil
+            self.builder = nil
+            onStateChange?(.failed(error.localizedDescription))
+            throw error
+        }
+    }
+
+    public func start(activity: HKWorkoutActivityType = .other) throws {
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = activity
+        configuration.locationType = .outdoor
+
+        Task {
+            try await start(configuration: configuration, mirrorToCompanion: false)
+        }
+    }
+
+    public func pause() {
+        session?.pause()
+    }
+
+    public func resume() {
+        session?.resume()
     }
 
     public func stop() {
+        guard session != nil else { return }
+        onStateChange?(.ending)
         session?.end()
     }
 
@@ -52,17 +108,37 @@ public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKL
         from fromState: HKWorkoutSessionState,
         date: Date
     ) {
-        if toState == .ended {
+        switch toState {
+        case .running:
+            onStateChange?(.running)
+        case .paused:
+            onStateChange?(.paused)
+        case .ended:
+            let builder = builder
             builder?.endCollection(withEnd: date) { [weak self] _, _ in
-                self?.builder?.finishWorkout { _, _ in }
+                builder?.finishWorkout { workout, error in
+                    guard let self else { return }
+                    if let error {
+                        self.onStateChange?(.failed(error.localizedDescription))
+                    } else {
+                        self.onStateChange?(.ended)
+                    }
+                    self.onWorkoutFinished?(workout)
+                    self.session = nil
+                    self.builder = nil
+                }
             }
+        default:
+            break
         }
     }
 
     public func workoutSession(
         _ workoutSession: HKWorkoutSession,
         didFailWithError error: Error
-    ) {}
+    ) {
+        onStateChange?(.failed(error.localizedDescription))
+    }
 
     public func workoutBuilder(
         _ workoutBuilder: HKLiveWorkoutBuilder,
@@ -73,6 +149,7 @@ public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKL
               let statistics = workoutBuilder.statistics(for: heartRate),
               let value = statistics.mostRecentQuantity()
         else { return }
+
         let bpm = value.doubleValue(
             for: HKUnit.count().unitDivided(by: .minute())
         )
@@ -82,5 +159,9 @@ public final class WatchWorkoutRecorder: NSObject, HKWorkoutSessionDelegate, HKL
     public func workoutBuilderDidCollectEvent(
         _ workoutBuilder: HKLiveWorkoutBuilder
     ) {}
+
+    public enum RecorderError: Error {
+        case alreadyRunning
+    }
 }
 #endif
