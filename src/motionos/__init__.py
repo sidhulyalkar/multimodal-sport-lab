e763f3c82ede0158535f19001bc85e9a3eb9ee95
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-21 17:07:12 -0700

    feat: add MotionOS M0 core contracts

---
+++ src/motionos/__init__.py
@@ -0,0 +1,6 @@
+"""MotionOS M0 multimodal capture reference implementation."""
+
+from .schema import DeviceDescriptor, SensorEvent, SessionManifest
+
+__all__ = ["DeviceDescriptor", "SensorEvent", "SessionManifest"]
+__version__ = "0.1.0"
