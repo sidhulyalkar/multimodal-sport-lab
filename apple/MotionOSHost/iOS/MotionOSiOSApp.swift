1a252c407a69c2ef788080670504f3091120d0b4
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-21 19:50:18 -0700

    feat: provide equipment pod controller to iPhone UI

--- apple/MotionOSHost/iOS/MotionOSiOSApp.swift
+++ apple/MotionOSHost/iOS/MotionOSiOSApp.swift
@@ -3,12 +3,14 @@ import SwiftUI
 @main
 struct MotionOSiOSApp: App {
     @StateObject private var coordinator = PhoneSessionCoordinator()
+    @StateObject private var podController = EquipmentPodController()
 
     var body: some Scene {
         WindowGroup {
             PhoneContentView()
                 .environmentObject(coordinator)
                 .environmentObject(coordinator.inbox)
+                .environmentObject(podController)
         }
     }
 }
